6c026a3e02671d0947ee3755a270850ee226ee1a
Prasanna187 <154608607+Prasanna187@users.noreply.github.com>
2024-04-14 13:18:48 +0530

    Initial Commit

---
+++ LoadingImage/LoadingImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingImageApp.swift
+//  LoadingImage
+//
+//  Created by Prasanna on 14/04/24.
+//
+
+import SwiftUI
+
+@main
+struct LoadingImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
